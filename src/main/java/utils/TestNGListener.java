d513a2cc38a10725461976facdc4946c55944e33
Diana Surovtseva <dia.surovtseva@gmail.com>
2020-08-18 22:24:18 +0300

    refactoring 3.0

--- src/main/java/utils/TestNGListener.java
+++ src/main/java/utils/TestNGListener.java
@@ -1,36 +1,13 @@
 package utils;
 
-import org.openqa.selenium.OutputType;
-import org.openqa.selenium.TakesScreenshot;
 import org.testng.ITestContext;
 import org.testng.ITestListener;
 import org.testng.ITestResult;
-
-import java.io.File;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.nio.file.StandardCopyOption;
-import utils.WebDriverFactory;
 
 public class TestNGListener implements ITestListener {
 
     @Override
     public void onTestFailure(ITestResult result) {
-        File screenshotsFolder = new File(System.getProperty("user.dir") + "/screenshots");
-        if (!screenshotsFolder.exists()) {
-            screenshotsFolder.mkdir();
-        }
-        File screenshot = captureScreenshot();
-        Path pathToScreenshot = Paths.get(screenshot.getPath());
-        try {
-            String currTime = java.time.LocalTime.now().toString().replace(":", ".");
-            String screenshotName = screenshotsFolder + "/" + "Screenshot_" + currTime + ".png";
-            Files.copy(pathToScreenshot, Paths.get(screenshotName), StandardCopyOption.COPY_ATTRIBUTES);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
 
     }
 
@@ -54,10 +31,6 @@ public class TestNGListener implements ITestListener {
 
     }
 
-    private File captureScreenshot(){
-        return ((TakesScreenshot) WebDriverFactory.getDriver()).getScreenshotAs(OutputType.FILE);
-    }
-
     @Override
     public void onTestStart(ITestResult result) {
 //        String browserName = result.getTestContext().getCurrentXmlTest().getParameter("browserName");
@@ -66,19 +39,6 @@ public class TestNGListener implements ITestListener {
 
     @Override
     public void onTestSuccess(ITestResult iTestResult) {
-        File screenshotsFolder = new File(System.getProperty("user.dir") + "/screenshots");
-        if (!screenshotsFolder.exists()) {
-            screenshotsFolder.mkdir();
-        }
-        File screenshot = captureScreenshot();
-        Path pathToScreenshot = Paths.get(screenshot.getPath());
-        try {
-            String currTime = java.time.LocalTime.now().toString().replace(":", ".");
-            String screenshotName = screenshotsFolder + "/" + "Screenshot_" + currTime + ".png";
-            Files.copy(pathToScreenshot, Paths.get(screenshotName), StandardCopyOption.COPY_ATTRIBUTES);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
     }
 }
 
